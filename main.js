f63f3610475526b057f46d000a1de9002cd99590
LeonardoDib <130074672+LeonardoDib@users.noreply.github.com>
2024-04-09 11:12:29 -0300

    dfgdfgdfg

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
+             botoes[j].classList.remove("ativo");
+             textos[j].classList.remove("ativo");
+       }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("")
+
+contadores[0].textContent = ;
